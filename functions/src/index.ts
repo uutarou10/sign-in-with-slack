286f9513a70475f3add854f11de0880c9563531b
Kota Nonaka <kota_nonaka@fringe81.com>
2019-08-26 09:16:32 +0900

    tokenのせてredirect

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,13 @@
 import * as functions from 'firebase-functions';
 import * as admin from 'firebase-admin';
 import { oauthAccess } from './slack';
+import { URL } from 'url';
 
 admin.initializeApp();
 
 exports.authWithSlack = functions.https.onRequest(async (req, res) => {
   const slackAuthCode = req.query.code as string | undefined;
+  const redirectUri = req.query.state as string | undefined;
 
   if (!slackAuthCode) {
     console.warn('code query string not find.')
@@ -22,9 +24,16 @@ exports.authWithSlack = functions.https.onRequest(async (req, res) => {
 
   try {
     const customToken = await admin.auth().createCustomToken(userCredential.user_id);
-    res.json({
-      custom_token: customToken
-    }).end();
+
+    if (redirectUri) {
+      const url = new URL(redirectUri);
+      url.search = `t=${customToken}`;
+      res.redirect(303, url.toString());
+    } else {
+      res.json({
+        custom_token: customToken
+      }).end();
+    }
     return;
   } catch (e) {
     console.error('Failed to create custom token:', e)
